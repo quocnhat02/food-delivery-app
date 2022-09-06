a95946dca5931ee71b9d3bb75027fc31ec10a290
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 22:29:09 +0700

    recode

--- src/index.js
+++ src/index.js
@@ -18,26 +18,19 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg"
+        alt=""
+      />
+      <h1>I love you to the moon</h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        Nguyen Quoc Nhat
+      </h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I love you to the moon</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}>
-    Nguyen Quoc Nhat
-  </h4>
-);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
