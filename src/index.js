94098500546a95e6e2cb7b6ce47408f2ec1b087a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 20:40:06 +0700

    this is a booklist

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h4>this is John and this is my first component</h4>;
+function BookList() {
+  return <section>this is a booklist</section>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<BookList />);
