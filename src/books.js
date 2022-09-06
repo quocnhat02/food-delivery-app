2ed3d842f02774bb64fe1f39fd7b2be31c700f7e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 23:54:04 +0700

    import and export 1

---
+++ src/books.js
@@ -0,0 +1,15 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg",
+    title: "I love you to the moon",
+    author: "Nguyen Quoc Nhat",
+  },
+
+  {
+    id: 2,
+    img: "https://media.geeksforgeeks.org/img-practice/Layer2(1)-1637066767.svg",
+    title: "Our class is my family",
+    author: "John Wick",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -3,21 +3,7 @@ import ReactDOM from "react-dom/client";
 
 // CSS
 import "./index.css";
-const books = [
-  {
-    id: 1,
-    img: "https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg",
-    title: "I love you to the moon",
-    author: "Nguyen Quoc Nhat",
-  },
-
-  {
-    id: 2,
-    img: "https://media.geeksforgeeks.org/img-practice/Layer2(1)-1637066767.svg",
-    title: "Our class is my family",
-    author: "John Wick",
-  },
-];
+import { books } from "./books";
 
 function BookList() {
   return (
