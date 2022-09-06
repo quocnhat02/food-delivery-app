43b3cb3f9e6040c930673718c1ae099506ce88dd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 23:38:10 +0700

    event basics 1

--- src/index.js
+++ src/index.js
@@ -30,12 +30,19 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert("hello world");
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Alert message
+      </button>
     </article>
   );
 };
