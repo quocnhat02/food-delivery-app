3941f25e9c53341b56e358b62bd0eb3048a74694
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-07 00:00:25 +0700

    import and export 3

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,33 @@
 import React from "react";
 
-const Book = () => {
-  return <div></div>;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    alert("hello world");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
+  return (
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(author);
+      }}
+    >
+      <img src={img} alt="" />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        Alert message
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More Complete Example
+      </button>
+    </article>
+  );
 };
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 
 // CSS
 import "./index.css";
+
 import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
@@ -14,36 +16,6 @@ function BookList() {
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  // onClick, onMouseOver
-  const clickHandler = (e) => {
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(author);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        Alert message
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        More Complete Example
-      </button>
-    </article>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
