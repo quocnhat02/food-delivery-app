7e44ea9a8d8efc9de833a1ff665a0d494ea13d1c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 23:39:49 +0700

    event basics 2

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
       <button type="button" onClick={clickHandler}>
         Alert message
