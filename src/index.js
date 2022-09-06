6b662fd52d129c8b60b0b5d541e3d6f68e75feb7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 23:15:27 +0700

    simple list 1

--- src/index.js
+++ src/index.js
@@ -1,52 +1,38 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 // CSS
 import "./index.css";
-// setup vars
-const firstBook = {
-  img: "https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg",
-  title: "I love you to the moon",
-  author: "Nguyen Quoc Nhat",
-};
+const books = [
+  {
+    img: "https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg",
+    title: "I love you to the moon",
+    author: "Nguyen Quoc Nhat",
+  },
 
-const secondBook = {
-  img: "https://media.geeksforgeeks.org/img-practice/Layer2(1)-1637066767.svg",
-  title: "Our class is my family",
-  author: "John Wick",
-};
+  {
+    img: "https://media.geeksforgeeks.org/img-practice/Layer2(1)-1637066767.svg",
+    title: "Our class is my family",
+    author: "John Wick",
+  },
+];
+
+const names = ["john", "peter", "susan"];
+const newName = names.map((name) => {
+  console.log(name);
+});
 
 function BookList() {
-  return (
-    <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio quod
-          dolor amet praesentium vitae at exercitationem voluptatibus!
-          Doloribus, officiis atque.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="bookList">{names}</section>;
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props;
+  const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
