ebee40090d2a77d7e33f565c17a29552c73d8f02
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 20:52:51 +0700

    image and title

--- src/index.js
+++ src/index.js
@@ -2,8 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 function BookList() {
-  return <section>this is a booklist</section>;
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>I love you to the moon</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<BookList />);
