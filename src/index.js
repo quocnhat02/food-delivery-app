c139c8a38406dc036c5b966b8021bf4891fc3c80
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-06 23:29:03 +0700

    key props

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 const books = [
   {
+    id: 1,
     img: "https://media.geeksforgeeks.org/img-practice/Layer2-1637062356.svg",
     title: "I love you to the moon",
     author: "Nguyen Quoc Nhat",
   },
 
   {
+    id: 2,
     img: "https://media.geeksforgeeks.org/img-practice/Layer2(1)-1637066767.svg",
     title: "Our class is my family",
     author: "John Wick",
@@ -21,7 +23,7 @@ function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
