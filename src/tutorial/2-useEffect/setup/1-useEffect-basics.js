a8312401c172f2dc0c0a29aba7e1d58cea5dd57d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 20:33:36 +0700

    useEffect-basic 2

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  useEffect(() => {
+    console.log("call useEffect");
+  });
+
+  console.log("render component");
+
   return <h2>useEffect Basics</h2>;
 };
 
