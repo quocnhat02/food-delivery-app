37f65f5297b77d6c6496995a2db67cc0283828cd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 19:05:05 +0700

    useState-basics 4

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  console.log(useState("Hello World"));
-  const value = useState(1)[0];
-  const handler = useState(1)[1];
-  console.log(value, handler);
-  return <h2>useState basic example</h2>;
+  // console.log(useState("Hello World"));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState("123");
+
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
