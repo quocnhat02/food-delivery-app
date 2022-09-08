b6dbfffe4bd9989106291045328edc73711eb964
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 22:18:14 +0700

    multiple returns-fetching example 1

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
-  if (loading) {
+  if (isLoading) {
     return <h2>Loading...</h2>;
   }
   return <h2>multiple returns</h2>;
