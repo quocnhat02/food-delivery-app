2a5d9aff271d5b2fe1ce73ce4b44e0edad36986e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 18:48:39 +0700

    useState-simple use case 3

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const ErrorExample = () => {
   let title = "random title";
+
+  const handleClick = () => {
+    console.log(title);
+  };
+
   return (
     <React.Fragment>
       <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change title
+      </button>
     </React.Fragment>
   );
 };
