437ef3d2279a8594038798f14ce6d086eb3d1ff7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 22:25:23 +0700

    multiple returns-fetching example 2

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -3,9 +3,22 @@ const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
 
   if (isLoading) {
-    return <h2>Loading...</h2>;
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
   }
   return <h2>multiple returns</h2>;
 };
