9c909049da355d90f0a059dda5cbe50eee20c271
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 19:44:54 +0700

    useState-array example 5

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,12 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+
   return (
     <>
       {people.map((person) => {
@@ -10,6 +16,7 @@ const UseStateArray = () => {
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>
           </div>
         );
       })}
