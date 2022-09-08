86cbd0a007f2064c943b6d3b412e4afed0d717ac
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 19:23:38 +0700

    useState-array example 2

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 import { data } from "../../../data";
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState([]);
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        console.log(person);
+        return <h3>Hello</h3>;
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
