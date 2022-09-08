2fd75214fa9cdf0a78d7a548f813399994b1f09b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-08 21:05:38 +0700

    useEffect-cleanup function 2

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  console.log(size);
   return <h2>useEffect cleanup</h2>;
 };
 
