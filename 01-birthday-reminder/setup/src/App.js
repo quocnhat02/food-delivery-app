398366f327dbfdf564c7386a1fc008b23ce74f0a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-09 21:52:21 +0700

    begin project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <h2>reminder project-01 setup</h2>;
 }
 
 export default App;
