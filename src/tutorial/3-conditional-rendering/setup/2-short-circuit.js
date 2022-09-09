8f3a1a00f946e5fd0ffc1aadc25f7c239f7a9db6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-09 10:26:22 +0700

    ternary oparator 2

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -20,8 +20,17 @@ const ShortCircuit = () => {
       }
     } */}
       <h1>{text || "john doe"}</h1>
-      <button className="btn">toggle error</button>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
       {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
     </>
   );
 };
