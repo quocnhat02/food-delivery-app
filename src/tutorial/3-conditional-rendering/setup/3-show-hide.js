ab9794e394a8e8e3861e156a2553a222cf7c30bd
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-09 10:32:28 +0700

    show hide component 4

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -14,6 +14,16 @@ const ShowHide = () => {
 };
 
 const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+  }, []);
+
   return (
     <div style={{ marginTop: "2rem" }}>
       <h1>window</h1>
