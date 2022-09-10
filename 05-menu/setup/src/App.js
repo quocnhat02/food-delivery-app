4deb6dca27b0e4089c6f5f12ab4b673c3eee2907
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 12:03:53 +0700

    menu-display items 7

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu section">
