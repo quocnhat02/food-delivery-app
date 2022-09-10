ee80e5eb4b381fc337165d913b6ce78d57aba601
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 10:25:57 +0700

    reviews-basic setup 3

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
+  const [index, setIndex] = useState(0);
+  console.log(people);
   return <h2>review component</h2>;
 };
 
