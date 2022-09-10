6620473c5213a7a61520e351ebe7d4fbb92d2c5b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 12:06:55 +0700

    menu-display items 8

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
