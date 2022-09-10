b0de3d6324edb6ab99af699adfae277feda864de
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-10 11:36:22 +0700

    accordion-complete 5

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(true);
   return (
     <article className="question">
       <header>
         <h4>{title}</h4>
         <button className="btn">btn</button>
       </header>
-      <p>{info}</p>
+      {showInfo && <p>{info}</p>}
     </article>
   );
 };
