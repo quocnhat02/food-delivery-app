51ca914570ac6b76736b104f6090afd1632ba878
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-11 20:01:45 +0700

    cocktails-display list 3

--- 15-cocktails/setup/src/components/Cocktail.js
+++ 15-cocktails/setup/src/components/Cocktail.js
@@ -11,6 +11,9 @@ const Cocktail = ({ image, name, id, info, glass }) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
+        <Link to={`/cocktail/${id}`} className="btn btn-primary">
+          details
+        </Link>
       </div>
     </article>
   );
