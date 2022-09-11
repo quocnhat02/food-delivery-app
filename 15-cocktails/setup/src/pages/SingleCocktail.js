30b3295fd6acf0c0e2acd61e3ff4164b1ee8c1e2
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-11 20:22:44 +0700

    cocktails-single cocktail page 2

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -5,6 +5,22 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
   const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [id]);
+
   return (
     <div>
       <h2>{id} </h2>
