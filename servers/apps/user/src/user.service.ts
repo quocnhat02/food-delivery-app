5159a2b14945a217819f1a67c8dbe7c1e3cb0890
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-16 14:54:16 +0700

    user service

--- servers/apps/user/src/user.service.ts
+++ servers/apps/user/src/user.service.ts
@@ -1,8 +1,33 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // private readonly prisma
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register user service
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = { name, email, password };
+    return user;
+  }
+
+  // login user service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = { email, password };
+    return user;
+  }
+
+  // get all users service
+  async getUsers() {
+    const users = [];
+    return users;
   }
 }
