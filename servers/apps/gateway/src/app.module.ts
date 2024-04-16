cefafc929722025ed849520ae68c5d2037c42cb9
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-16 14:32:00 +0700

    user dto

--- servers/apps/gateway/src/app.module.ts
+++ servers/apps/gateway/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [],
+        }),
+      },
+    }),
+  ],
   controllers: [],
   providers: [AppService],
 })
--- servers/apps/user/src/user.module.ts
+++ servers/apps/user/src/user.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, ConfigService, JwtService],
 })
 export class UserModule {}
